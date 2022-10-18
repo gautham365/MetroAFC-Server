7c446e4c9dc3003df2e2ce7c61468b9778d59718
gautham-d <102036884+gautham-d@users.noreply.github.com>
2022-10-19 01:09:56 +0530

    sudo for other commands

--- pm2_runner.sh
+++ pm2_runner.sh
@@ -3,7 +3,7 @@ sudo pm2 describe metro-server > /dev/null
 RUNNING=$?
 
 if [ "${RUNNING}" -ne 0 ]; then
-          pm2 restart metro-server
-else
           sudo npm install -g pm2 && pm2 start npm --name "metro-server" -- start
+else
+          sudo pm2 restart metro-server
 fi;
