35db65bc1aa869f165dd55fcc8b05d6ccf2dffa7
gautham-d <102036884+gautham-d@users.noreply.github.com>
2022-10-22 00:10:32 +0530

    added webhook urls

--- routes.js
+++ routes.js
@@ -16,5 +16,8 @@ router.post('/journeys/all', controller.isLoggedIn, controller.getAllJourneys);
 router.post('/entry', controller.isLoggedIn ,controller.entry);
 router.post('/exit', controller.isLoggedIn ,controller.exit);
 
+router.get('/webhook/payment/finalStatus', controller.finalStatusWH);
+router.post('/webhook/payment/finalStatus', controller.finalStatusWHP);
+
 // export the routes
 module.exports = router;
